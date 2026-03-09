91be37cfafb18f4db7e9331f6bc6585e90436fea
mrjhon <joaofelipeazevedo28@gmail.com>
2026-03-09 13:27:44 -0400

    modified:   controllers/users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,20 @@
 const express = require("express")
 const User = require("../models/users")
 
-const createUser = (req,res) =>{
+const createUser = async (req,res) =>{
     const username = req.body.username
+    const pwd = req.body.password
+    const email = req.body.email
+    const role = req.body.role
+
+    if (!username || !pwd || !email || !role) return res.status(401)
+    
+    try {
+        const pwdHash = await bcrypt.hash(pwd,10)
+        const newUser = await User.create({"name":username,"password":pwdHash,"email":email,"role":role})
+        return res.status(200).json({"message":newUser})
+    } catch (err){
+        return res.status(500).json({"message":err.message})
+    }
 }
+
