cf543ccdf953a6afcd2fc6021c70c979b4269c2f
mrjhon <joaofelipeazevedo28@gmail.com>
2026-03-09 14:10:47 -0400

    modified:   middlewares/users.js 	modified:   routes/users.js

--- middlewares/users.js
+++ middlewares/users.js
@@ -19,3 +19,6 @@ const verify = async (req,res,next)=>{
     }
     
 }
+
+
+module.exports = verify
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const {updateUser , deleteUser , acessUser , createUser  } = require("../controllersqusers")
-
+const verify = require("../middlewares/users")
 
 /*
 post new_user
@@ -12,6 +12,7 @@ patch user
 
 router.post("/new-user",createUser)
 router.get("/user",acessUser)
-router.delete("/user/:id",deleteUser)
-router.patch("/user/:id",updateUser)
+router.delete("/user/:id",verify,deleteUser)
+router.patch("/user/:id",verify,updateUser)
 
+module.exports = router
