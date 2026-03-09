f1ed43c72ab45c0a9490b385981c90eb2eaa61c4
mrjhon <joaofelipeazevedo28@gmail.com>
2026-03-09 14:09:11 -0400

    modified:   middlewares/users.js

---
+++ middlewares/users.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+
+require("dotenv").config()
+
+const verify = async (req,res,next)=>{
+    const header = req.header.authorization
+    if (!header) return res.status(402)
+    const token = header.split(" ")[1]
+    try{
+    jwt.verify(token , process.env.AcessToken,
+        (err,user)=>{
+            if (err) return res.status(403)
+            req.user = user
+            next()
+        }
+    )
+    }catch(err){
+        return res.status(500).json({"message":err.message})
+    }
+    
+}
