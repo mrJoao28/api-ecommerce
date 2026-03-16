982df68c8b8bad166e22b5c5599705aa346506b7
mrjhon <joaofelipeazevedo28@gmail.com>
2026-03-16 08:50:07 -0400

    new file:   middlewares/products.js 	modified:   routes/products.js

---
+++ middlewares/products.js
@@ -0,0 +1,16 @@
+
+
+
+const checkUser = (req,res,next)=>{
+    const role = req.user.role
+    if (!role) return res.sendStatus(401)
+
+    if ( role === "adm") next()
+    
+    else {
+        return "nao permitido"
+    }
+
+}
+
+module.exports  = checkUser
--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,13 @@ const express = require("express")
 const router = express.Router()
 const {updateProduct,deleteProduct,showAllProducts,showProduct,creatProduct} = require("../controllers/products")
 const verify = require("../middlewares/users")
+const checkUser = require("../middlewares/products")
 
 
 router.get("/products",verify,showAllProducts)
 router.get("/products/:id",verify,showProduct)
-router.post("/create-product",verify,creatProduct)
-router.patch("/update-product",verify,updateProduct)
-router.delete("/delete-product",verify,deleteProduct)
+router.post("/create-product",checkUser,verify,creatProduct)
+router.patch("/update-product",checkUser,verify,updateProduct)
+router.delete("/delete-product",checkUser,verify,deleteProduct)
 
 module.exports = router 
