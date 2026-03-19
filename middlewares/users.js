c86bbfe28f4dc18794acbf2975ad888bda13429f
mrjhon <joaofelipeazevedo28@gmail.com>
2026-03-19 07:05:38 -0400

    modified:   middlewares/users.js

--- middlewares/users.js
+++ middlewares/users.js
@@ -21,12 +21,4 @@ const verify = async (req,res,next)=>{
     
 }
 
-
-const verify_adm = (req,res,next)=>{
-    const role = req.user.role 
-    if (!role) return res.sendStatus(401)
-    if (role !== "adm") return res.sendStatus(402)
-    else next()
-}
-
-module.exports = {verify , verify_adm}
+module.exports = {verify}
